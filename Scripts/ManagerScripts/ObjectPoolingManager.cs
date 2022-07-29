bb9fabaa3c5e99f5dee3ba71f11537d5be81520c
kidagine <davidkalatzis1999@hotmail.com>
2022-07-29 21:27:24 +0300

    Update pooling manager

--- Scripts/ManagerScripts/ObjectPoolingManager.cs
+++ Scripts/ManagerScripts/ObjectPoolingManager.cs
@@ -7,7 +7,7 @@ namespace Demonics.Manager
 	{
 		[SerializeField] private List<ObjectPool> _objectPools;
 		[SerializeField] private Dictionary<GameObject, Queue<GameObject>> _objectPoolDictionary = new Dictionary<GameObject, Queue<GameObject>>();
-
+		private List<GameObject> _objects = new List<GameObject>();
 		public static ObjectPoolingManager Instance { get; private set; }
 
 
@@ -26,6 +26,7 @@ namespace Demonics.Manager
 					GameObject poolObject = Instantiate(objectPool.prefab, transform);
 					poolObject.SetActive(false);
 					objectPoolQueue.Enqueue(poolObject);
+					_objects.Add(poolObject);
 				}
 				_objectPoolDictionary.Add(objectPool.prefab, objectPoolQueue);
 			}
@@ -43,13 +44,14 @@ namespace Demonics.Manager
 			}
 		}
 
-		public GameObject Spawn(GameObject newPoolObject, Vector3 position = default, Quaternion rotation = default)
+		public GameObject Spawn(GameObject newPoolObject, Vector3 position = default, Quaternion rotation = default, Transform parent = default)
 		{
 			if (_objectPoolDictionary.ContainsKey(newPoolObject))
 			{
 				GameObject poolObject = _objectPoolDictionary[newPoolObject].Dequeue();
 				poolObject.transform.position = position;
 				poolObject.transform.rotation = rotation;
+				poolObject.transform.parent = parent;
 				poolObject.SetActive(true);
 				_objectPoolDictionary[newPoolObject].Enqueue(poolObject);
 				return poolObject;
@@ -58,6 +60,17 @@ namespace Demonics.Manager
 			{
 				Debug.LogError($"Pool with name {newPoolObject.name} doesn't exist.");
 				return null;
+			}
+		}
+
+		public void DisableAllObjects()
+		{
+			for (int i = 0; i < _objects.Count; i++)
+			{
+				if (_objects[i] != null)
+				{
+					_objects[i].SetActive(false);
+				}
 			}
 		}
 	}
