60c9c1e878c32bff6a5c01a5001ee28361408113
David Kalatzis <davidkalatzis1999@hotmail.com>
2022-12-14 15:19:03 +0200

    Update ObjectPoolingManager.cs

--- Scripts/ManagerScripts/ObjectPoolingManager.cs
+++ Scripts/ManagerScripts/ObjectPoolingManager.cs
@@ -3,75 +3,65 @@ using UnityEngine;
 
 namespace Demonics.Manager
 {
-	public class ObjectPoolingManager : MonoBehaviour
-	{
-		[SerializeField] private List<ObjectPool> _objectPools;
-		[SerializeField] private Dictionary<GameObject, Queue<GameObject>> _objectPoolDictionary = new Dictionary<GameObject, Queue<GameObject>>();
-		private List<GameObject> _objects = new List<GameObject>();
-		public static ObjectPoolingManager Instance { get; private set; }
+public class ObjectPoolingManager : MonoBehaviour
+{
+	[SerializeField] private List<ObjectPool> _objectPools;
+	[SerializeField] private Dictionary<string, Queue<GameObject>> _objectPoolDictionary = new Dictionary<string, Queue<GameObject>>();
+	private List<GameObject> _objects = new List<GameObject>();
+	public static ObjectPoolingManager Instance { get; private set; }
 
 
-		void Awake()
-		{
-			CheckInstance();
-		}
+        void Awake()
+        {
+            CheckInstance();
+        }
 
-		void Start()
-		{
-			foreach (ObjectPool objectPool in _objectPools)
-			{
-				Queue<GameObject> objectPoolQueue = new Queue<GameObject>();
-				for (int i = 0; i < objectPool.size; i++)
-				{
-					GameObject poolObject = Instantiate(objectPool.prefab, transform);
-					poolObject.SetActive(false);
-					objectPoolQueue.Enqueue(poolObject);
-					_objects.Add(poolObject);
-				}
-				_objectPoolDictionary.Add(objectPool.prefab, objectPoolQueue);
-			}
-		}
+        void Start()
+        {
+            foreach (ObjectPool objectPool in _objectPools)
+            {
+                Queue<GameObject> objectPoolQueue = new Queue<GameObject>();
+                for (int i = 0; i < objectPool.size; i++)
+                {
+                    GameObject poolObject = Instantiate(objectPool.prefab, transform);
+                    poolObject.SetActive(false);
+                    objectPoolQueue.Enqueue(poolObject);
+                    _objects.Add(poolObject);
+                }
+                _objectPoolDictionary.Add(objectPool.prefab.name, objectPoolQueue);
+            }
+        }
 
-		private void CheckInstance()
-		{
-			if (Instance != null && Instance != this)
-			{
-				Destroy(gameObject);
-			}
-			else
-			{
-				Instance = this;
-			}
-		}
+        private void CheckInstance()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
 
-		public GameObject Spawn(GameObject newPoolObject, Vector3 position = default, Quaternion rotation = default, Transform parent = default)
-		{
-			if (_objectPoolDictionary.ContainsKey(newPoolObject))
-			{
-				GameObject poolObject = _objectPoolDictionary[newPoolObject].Dequeue();
-				poolObject.transform.position = position;
-				poolObject.transform.rotation = rotation;
-				poolObject.transform.parent = parent;
-				poolObject.SetActive(true);
-				_objectPoolDictionary[newPoolObject].Enqueue(poolObject);
-				return poolObject;
-			}
-			else
-			{
-				Debug.LogError($"Pool with name {newPoolObject.name} doesn't exist.");
-				return null;
-			}
-		}
+        public void SetObject(string name, bool active)
+        {
+            if (_objectPoolDictionary.ContainsKey(name))
+            {
+                GameObject poolObject = _objectPoolDictionary[name].Dequeue();
+                poolObject.SetActive(active);
+            }
+        }
 
-		public void DisableAllObjects()
-		{
-			for (int i = 0; i < _objects.Count; i++)
-			{
-				if (_objects[i] != null)
-				{
-					_objects[i].SetActive(false);
-				}
-			}
-		}
-	}
+        public void DisableAllObjects()
+        {
+            for (int i = 0; i < _objects.Count; i++)
+            {
+                if (_objects[i] != null)
+                {
+                    _objects[i].SetActive(false);
+                }
+            }
+        }
+    }
 }
